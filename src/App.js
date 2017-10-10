cd3dd68720c0e101a32b8f6939d158b3eb55b452
Bina Khalid <binaanwar@gmail.com>
2017-10-10 12:46:05 +0300

    incomplete

--- src/App.js
+++ src/App.js
@@ -16,11 +16,32 @@ state ={
     {recipeName: 'korma', ingredients:['chicken','yogurt','onion']},
     {recipeName: 'Birayni', ingredients:['chicken','yogurt','onion','tomato']},
     {recipeName: 'sabzi', ingredients:['Aalo','gobi','onion']}
-
-  ]
+  ],
+  showAdd: false,
+   newestRecipe: {recipeName:"", ingredients:[]}
 }
+// delete a recipe
 deteleRecipe(index){
+  let recipes = this.state.recipes.slice();
+  recipes.splice(index, 1);
+  this.setState({recipes});
+}
+//Update updateNewRecipe
+updateNewRecipe(recipeName, ingredients){
+  this.setState({newestRecipe:{recipeName: recipeName, ingredients: ingredients}});
 
+
+}
+
+//Closes a modal
+close =() => {
+  if(this.state.showAdd){
+    this.setState({showAdd:false})
+  }
+}
+// Open a modal
+open  = (state) => {
+  this.setState({[state]:true});
 
 }
   render() {
@@ -44,7 +65,36 @@ deteleRecipe(index){
 
         ))}
       </Accordion>
-      <button bsStyle="primary">
+<Modal show={this.state.showAdd} onHide={this.close}>
+<Modal.Header closeButton>
+  <Modal.Title>Add Recipe</Modal.Title>
+    <Modal.Body>
+      <FormGroup controlId='formBasicText'>
+        <ControlLabel>Recipe Name</ControlLabel>
+        <FormControl
+        type="text"
+        value={newestRecipe.recipeName}
+        placeholder='Enter Recipe Name'
+        onChange = {(event) => this.updateNewRecipe(event.target.value, newestRecipe.ingredients)}
+
+        ></FormControl>
+      </FormGroup>
+        <FormGroup controlId='formControlsTextarea'>
+        <ControlLabel>Recipe Name</ControlLabel>
+        <FormControl
+        type="textarea"
+        value = {newestRecipe.recipeName}
+        placeholder='Enter Ingredients (Seprate By Commas)'
+        onChange = {(event) => this.updateNewRecipe(newestRecipe.recipeName, event.target.value.split(","))}
+        value ={newestRecipe.ingredients}
+
+        ></FormControl>
+      </FormGroup>
+    </Modal.Body>
+</Modal.Header>
+
+</Modal>
+      <button bsStyle="primary" onClick={(event) => this.open("showAdd")}>
       Add Recipe
       </button>
         
